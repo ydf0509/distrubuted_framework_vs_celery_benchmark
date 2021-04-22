f4c573426c0a5336bbb7964552e287235fb8fbbb
yangdefeng <defeng.yang@skilnets.com>
2021-04-22 15:14:32 +0800

    commit

--- celery_benchmark/celery_consume_benchmark.py
+++ celery_benchmark/celery_consume_benchmark.py
@@ -9,24 +9,28 @@ from celery import platforms
 platforms.C_FORCE_ROOT = True
 # celery_app = celery.Celery('test_frame.test_celery.test_celery_app')
 celery_app = celery.Celery()
+
+
 class Config:
     broker_url = f'redis://:@127.0.0.1:6379/7'  # 使用redis
     task_routes = {
         'task_fun': {"queue": "celery_benchmark", },
     }
 
+
 celery_app.config_from_object(Config)
+
 
 @celery_app.task(name='task_fun')
 def task_fun(x):
     if x == 0:
-        print(time.strftime("%H:%M:%S"),'消费第一条')
+        print(time.strftime("%H:%M:%S"), '消费第一条')
     if x == 99999:
-        print(time.strftime("%H:%M:%S"),'消费第100000条')
+        print(time.strftime("%H:%M:%S"), '消费第100000条')
 
 
 if __name__ == '__main__':
     # 不需要使用命令行，直接启动此脚本。
     celery_app.worker_main(
-        argv=['worker', '--pool=gevent','--concurrency=50', '-n', 'worker1@%h', '--loglevel=INFO',
+        argv=['worker', '--pool=gevent', '--concurrency=50', '-n', 'worker1@%h', '--loglevel=INFO',
               '--queues=celery_benchmark'])
